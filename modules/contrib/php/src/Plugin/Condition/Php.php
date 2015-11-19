55c7ba1ea27b067c5edb5543295d65495510d34b
kazuko.murata <kazuko.murata.85@gmail.com>
2015-11-19 16:23:06 +0900

    add text filter modules (PHP, Code filter)

---
+++ modules/contrib/php/src/Plugin/Condition/Php.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\php\Plugin\Condition\Php.
+ */
+
+namespace Drupal\php\Plugin\Condition;
+
+use Drupal\Core\Annotation\Translation;
+use Drupal\Core\Condition\Annotation\Condition;
+use Drupal\Core\Condition\ConditionPluginBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Provides a 'Php' condition.
+ *
+ * @Condition(
+ *   id = "php",
+ *   label = @Translation("PHP")
+ * )
+ */
+class Php extends ConditionPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $form = parent::buildConfigurationForm($form, $form_state);
+    if (empty($this->configuration['php'])) {
+      // Initialize an empty value.
+      $this->configuration['php'] = FALSE;
+    }
+    $form['php'] = [
+      '#type' => 'textarea',
+      '#title' => t('When the following PHP return TRUE (experts only)'),
+      '#default_value' => $this->configuration['php'],
+      '#description' => t('Enter PHP code between <?php ?>. Note that executing incorrect PHP code can break your Drupal site. Return TRUE in order for this condition to evaluate as TRUE.'),
+      '#access' => \Drupal::currentUser()->hasPermission('use PHP for settings')
+    ];
+    return parent::buildConfigurationForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $this->configuration['php'] = $form_state->getValue('php');
+    parent::submitConfigurationForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function summary() {
+    if (!empty($this->configuration['php'])) {
+      return t('When the given PHP evaluates as @state.', ['@state' => !empty($this->configuration['negate']) ? 'FALSE' : 'TRUE']);
+    }
+    else {
+      return t('No PHP code has been provided.');
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function evaluate() {
+    return php_eval($this->configuration['php']);
+  }
+
+}
